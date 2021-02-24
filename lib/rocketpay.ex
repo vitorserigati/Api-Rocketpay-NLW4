01850f5bc8f5b2f7dd228654989e9b2f92d5cfc5
Vitor Serigati <vitor_serigati@hotmail.com>
2021-02-24 12:35:19 -0300

    [Aula 3] Transações com o banco de dados. Função Ecto.Multi

---
+++ lib/rocketpay.ex
@@ -0,0 +1,7 @@
+defmodule Rocketpay do
+  alias Rocketpay.Users.Create, as: UserCreate
+  alias Rocketpay.Account.Deposit
+
+  defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate deposit(params), to: Deposit, as: :call
+end
